635f7a8e392d126aa69c03df09cc041074fe1ecc
Andrey Antukh <niwi@niwi.nz>
2020-01-17 09:54:01 +0100

    Update changelog.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog #
 
+## Version 0.2.0 ##
+
+- Update promesa library.
+- Adapt to latest changes on promesa.
+
+
 ## Version 0.1.1 ##
 
 - Properly merge prepopulated caches (@txus)
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/urania "0.1.1"
+(defproject funcool/urania "0.2.0"
   :description "Elegant and Efficient remote data access for Clojure(Script)"
   :url "https://github.com/funcool/urania"
   :license {:name "The MIT License"
@@ -25,6 +25,6 @@
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [org.clojure/clojurescript "1.10.597"]
-                                  [funcool/promesa "4.0.2"]
+                                  [funcool/promesa "5.0.0"]
                                   [datascript "0.13.2"]]
                    :plugins [[lein-cljsbuild "1.1.7"]]}})
